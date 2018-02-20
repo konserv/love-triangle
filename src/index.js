06e667da349adecadd05b8806124d0f505727b2a
konserv32 <konserv32@gmail.com>
2018-02-20 22:20:36 +0300

    search triangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,27 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+	var first = 0;
+
+	if (preferences.length<3) {return(0)};
+
+ 	while (preferences[first]>preferences.length) {
+ 		first++
+ 	};
+
+ 	var sumTr = 0;
+ 	var second;
+ 	var third;
+
+ 	while (first<preferences.length) {
+ 		second = preferences[first]-1;
+ 		third = preferences[second]-1;
+ 		if (second>first && third>first && preferences[third]-1==first) {
+ 			sumTr++
+ 		};
+ 		first++;
+ 	};
+
+ 	return(sumTr);
+
 };
